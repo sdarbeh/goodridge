b38b97be08fffa9dfd26cd7496013d2848cadfe1
Sam Darbeh <56693052+sdarbeh@users.noreply.github.com>
2021-02-03 23:23:35 -0600

    Updates

---
+++ precache-manifest.1a6cd580ef866afcddfdaf0eccde97b7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6dc88b9d32a02ab3161d2ea78a726783",
+    "url": "/index.html"
+  },
+  {
+    "revision": "34a24e1f22f5ac61b82a",
+    "url": "/static/css/2.fb346933.chunk.css"
+  },
+  {
+    "revision": "c7d3f147517e86267237",
+    "url": "/static/css/main.34ee0a59.chunk.css"
+  },
+  {
+    "revision": "34a24e1f22f5ac61b82a",
+    "url": "/static/js/2.1b1f61ce.chunk.js"
+  },
+  {
+    "revision": "f6ff2ea47821e4fe753073bcb134410e",
+    "url": "/static/js/2.1b1f61ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7d3f147517e86267237",
+    "url": "/static/js/main.573688e5.chunk.js"
+  },
+  {
+    "revision": "663a9d1bcf3048df3336",
+    "url": "/static/js/runtime-main.8d1c111d.js"
+  }
+]);
